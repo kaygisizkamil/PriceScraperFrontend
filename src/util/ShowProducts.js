6d816736c44a23e8bb1274bfaef3fcc104875e41
Kamil Kaygisiz <kamilkaygisizz@gmail.com>
2023-08-20 01:15:53 +0300

    Update api url's

--- src/util/ShowProducts.js
+++ src/util/ShowProducts.js
@@ -17,7 +17,7 @@ const Image = styled.img`
 const ProductCard = styled.div`
   margin-bottom: 20px;
 `;
-const API_URI = 'https://35.160.120.126';
+const API_URI = 'http://0.0.0.0:1000';
 
 const CheapestProducts = ({ searchQuery }) => {
   const [products, setProducts] = useState([]);
