6e541a15e6919887d98bb741094bc23d853282c6
Kamil Kaygisiz <kamilkaygisizz@gmail.com>
2023-08-20 07:09:09 +0300

    Update api url's

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BrandFilterContainer from './containers/BrandFilterContainer';
-import { useHistory } from 'react-router-dom';
 
-import { Redirect } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <Redirect from="/" to="/index.html" />
+    <div>
       <BrandFilterContainer />
-    </Router>
+    </div>
   );
 };
 
+export default App;
 export default App;
